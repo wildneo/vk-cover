0b027eaa5bdd6f71ebd58d11a6d0eb7ca27d11d6
Roman Oskolkov <wild_neo@mail.ru>
2017-09-06 12:12:48 +0500

    Add configuration file

---
+++ config.js
@@ -0,0 +1,42 @@
+module.exports = {
+	token: process.env.ACCESS_TOKEN,
+	app: {
+		id: process.env.APP_ID,
+		secret: process.env.APP_SECRET
+	},
+	group: {
+		id: 117271043
+	},
+	cover: {
+		height: 1590,
+		width: 400
+	},
+	time: {
+		delay: minutesToMilliseconds(process.env.DELAY_TIME),
+		current: getCurrentTime()
+	},
+	img: {
+		name: 'cover_',
+		format: '.png',
+		path: '/img/'
+	}
+};
+
+function getCurrentTime() {
+	var date = new Date();
+	var h = addZero(date.getHours());
+	var m = addZero(date.getMinutes());
+	var s = addZero(date.getSeconds());
+	var time = '[' + h + ':' + m + ':' + s + ']';
+	return time;
+}
+
+function addZero(i) {
+	if (i < 10) i = "0" + i;
+	return i;
+}
+
+function minutesToMilliseconds(i) {
+	i = i * 60 * 1000;
+	return i;
+}
